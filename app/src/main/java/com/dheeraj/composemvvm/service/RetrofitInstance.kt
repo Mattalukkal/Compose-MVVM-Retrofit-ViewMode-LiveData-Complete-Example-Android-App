4ac61ee76d398e5ac634b319cb1857b399c26e7a
VISHNURAJ-MR <359739@manappuram.com>
2024-07-27 17:03:01 +0530

    ok

--- app/src/main/java/com/dheeraj/composemvvm/service/RetrofitInstance.kt
+++ app/src/main/java/com/dheeraj/composemvvm/service/RetrofitInstance.kt
@@ -1,15 +1,20 @@
 package com.dheeraj.composemvvm.service
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "https://random-data-api.com/api/v2/"
+    private val logging: HttpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
+    private const val BASE_URL = "https://random-data-api.com/api/v2/"
+    private val client = OkHttpClient.Builder().addInterceptor(logging).build()
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
     }
 
